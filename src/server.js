4b15212e54c663b902def6389a72cdf3db2c963b
Chachu21 <tsecha6@gmail.com>
2024-06-02 19:13:40 +0300

    default route is added

--- src/server.js
+++ src/server.js
@@ -31,7 +31,9 @@ app.use(cors());
 // Increase the limit for request payload size
 app.use(bodyParser.json({ limit: "50mb" }));
 app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
-
+app.get("/", (req, res) => {
+  res.send("this is mentorship for all !");
+});
 // Serve static files
 
 // Define your routes here
